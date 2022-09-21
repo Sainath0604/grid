65d4ee2de3237e827627e518a36ff32d0dcc39f6
BUMBAIYA <amitchauhan263871@gmail.com>
2022-09-21 22:14:30 +0530

    dynamic cell size and click event

--- grid.js
+++ grid.js
@@ -1,4 +1,25 @@
-function myFunction_1 () {
-    document.getElementById("grid_1").style.backgroundColor = "yellow";
-    console.log("clicked")
+var BoardContainer = document.getElementById("BoardContainer");
+
+function CellButtonClicked(event)
+{
+    var Cell = document.getElementById(event.currentTarget.id)
+    Cell.style.backgroundColor = "blue";
+    Cell.style.color = "white";
+}
+
+function PopulateBoard(numOfCells)
+{
+    for (var i = 1; i < numOfCells + 1; i++)
+    {
+        var newCell = document.createElement("div");
+        newCell.classList.add("grid-item");
+        newCell.setAttribute("id", "cell-"+i)
+        newCell.innerHTML = i;
+        newCell.addEventListener("click", CellButtonClicked);
+        BoardContainer.appendChild(newCell);
+    }
+}
+
+window.onload = function() {
+    PopulateBoard(25);
 }
